39efecd679e63819ebe1163a7d27240d3f969320
m4r23 <marcosalv433@gmail.com>
2025-03-29 19:12:45 -0600

    add buho and gallo

---
+++ src/components/Buho/Buho.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Buho() {
+  return (
+    <div>Buho</div>
+  )
+}
---
+++ src/components/Gallo/Gallo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Gallo() {
+  return (
+    <div>Gallo</div>
+  )
+}
